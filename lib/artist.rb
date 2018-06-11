5aab3ac2815150dfd584b11dcac0de890ee9d98b
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-11 21:38:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,20 @@
 class Artist
   attr_accessor :name
-  @songs = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
   
   def songs
     @songs
   end
-
-  def add_song(song)
-
-  end
-
 
 
 end

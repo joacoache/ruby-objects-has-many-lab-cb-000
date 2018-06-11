2b49b03f944c9f4464c26739c0747e0ecd24b1ed
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-11 22:08:45 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author
+    @author
+  end
+
+  def author_name
+    @author == nil ? nil : @author
+  end
+
+
+
+end

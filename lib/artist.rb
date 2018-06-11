a5e6af2880cfc255be2e308dddf700c3a9b0a33f
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-11 21:39:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,9 +11,15 @@ class Artist
     song.artist = self
   end
 
-  
+
   def songs
     @songs
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
   end
 
 
